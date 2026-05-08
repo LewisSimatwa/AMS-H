604dcafd34acad7bdc6a0ddfe82edf8503a0597d
Lewis S <lewissimatwa@gmail.com>
2026-05-08 16:11:15 +0300

    fixed vite config"

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',      
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
-    host: true, // allow access from network
-    allowedHosts: ['arachidic-awedly-bell.ngrok-free.dev'], 
-    strictPort: true, 
+    host: true,
+    strictPort: true,
     proxy: {
       '/backend': {
         target: 'http://localhost:8000',
