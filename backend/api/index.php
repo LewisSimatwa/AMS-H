0e84e7ffb42c618811d195b9aa4103f383a1be4b
Lewis S <lewissimatwa@gmail.com>
2026-05-08 21:03:55 +0300

    fixed route

--- backend/api/index.php
+++ backend/api/index.php
@@ -4,10 +4,23 @@ require 'helpers.php';
 
 // Headers
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+
+$allowedOrigins = [
+    'https://ams-h.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000',
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header('Access-Control-Allow-Credentials: true');
+} else {
+    header('Access-Control-Allow-Origin: https://ams-h.vercel.app');
+}
+
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Institution-ID');
-header('Access-Control-Allow-Credentials: true');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
