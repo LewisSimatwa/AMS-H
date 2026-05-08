939ef04ad1db57fce568abd412c7d14946211749
Lewis S <lewissimatwa@gmail.com>
2026-05-08 22:36:29 +0300

    fixed api

--- frontend/src/features/users/UserManagement.jsx
+++ frontend/src/features/users/UserManagement.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import '../../styles/UserManagement.css';
-import API_BASE_URL from "./config";
 import API_BASE_URL from "../../api/config";
 
 
